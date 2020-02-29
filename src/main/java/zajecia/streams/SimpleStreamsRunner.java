898d917de9d976f7fbfdfa7cb382df0518948fc4
Patryk.Marcisz <Patryk.Marcisz@alior.pl>
2020-02-29 11:08:49 +0100

    zadanie 2 streamy

--- src/main/java/zajecia/streams/SimpleStreamsRunner.java
+++ src/main/java/zajecia/streams/SimpleStreamsRunner.java
@@ -1,7 +1,10 @@
 package zajecia.streams;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -27,6 +30,38 @@ public class SimpleStreamsRunner {
                 .filter(x -> x.length() > 5)
                 .map(x -> x.toUpperCase())
                 .collect(Collectors.toList());
+    }
+
+    static void ex2(){
+        Optional<String> any = Stream.of("first", "second", "third", "fourth", "fifth")
+                .filter(x -> x.length() > 7)
+                .findAny();
+
+        if(any.isPresent()){
+            String s = any.get();
+        }
+
+        String result = any.orElse("to zwracamy jesli tam bylo pusto");
+
+    }
+
+    static void ex2_alternative(){
+        List<String> stringList = new ArrayList<>();
+        stringList.add("first");
+        stringList.add("second");
+        stringList.add("third");
+        stringList.add("fourth");
+        stringList.add("fifth");
+//        stringList = new ArrayList<>(Arrays.asList("first", "second", "third", "fourth", "fifth"))
+
+        String result = null;
+        for(int i=0; i<stringList.size(); i++){
+            if(stringList.get(i).length() > 5){
+                result = stringList.get(i);
+                break;
+            }
+        }
+        System.out.println(result);
     }
 
 }
